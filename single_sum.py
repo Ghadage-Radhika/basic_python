a4ec9d6e9e84baa6c4c53a7bac1f2c7e46a2e72c
Ghadage-Radhika <149257253+Ghadage-Radhika@users.noreply.github.com>
2025-03-12 16:00:44 +0530

    Update single_sum.py

--- single_sum.py
+++ single_sum.py
@@ -21,12 +21,10 @@ Example
 
 """
 number = int(input("Enter a number: "))
-
 while number >= 10:
     digit_sum = 0
     while number > 0:
         digit_sum += number % 10
         number //= 10
     number = digit_sum
-
 print("Output:", number)
